c5e308223d8e906923d2c718f7e1bd8a980abf2f
Allyn Ralf Ledesma <110903853+nylla8444@users.noreply.github.com>
2025-09-14 18:08:23 +0800

    feat: added Separator component

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -1,3 +1,3 @@
-export default function Separator() {
-    return <div className="w-full h-px bg-tertiary-black mt-8 md:mr-5" />;
+export default function Separator({ className }) {
+    return <div className={`w-full h-px bg-tertiary-black mt-8 md:mr-5 ${className}`} />;
 }
